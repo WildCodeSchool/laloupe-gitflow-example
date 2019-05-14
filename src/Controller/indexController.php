2d93c6b4d113f77b12f5071c5238b98745a550eb
Richartrex <learic7@gmail.com>
2019-05-14 16:51:17 +0200

    Add function richard integration

--- src/Controller/indexController.php
+++ src/Controller/indexController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class indexController extends AbstractController
 {
-    public function benoitXavier()
+    public function richard()
     {
-        return $this->json(['name' => 'bx']);
+        return $this->json(['name' => 'richard']);
     }
 }
