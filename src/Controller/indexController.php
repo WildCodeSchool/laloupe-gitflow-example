6d5d8986d91d0838630f2f1bb57f9e57b47a027e
fabrice <47209499+Fabrice-Pinglot@users.noreply.github.com>
2019-05-14 17:07:01 +0200

    Update indexController.php

--- src/Controller/indexController.php
+++ src/Controller/indexController.php
@@ -8,6 +8,6 @@ class indexController extends AbstractController
 {
     public function Fabrice()
     {
-        return $this->json(['name' => 'bx']);
+        return $this->json(['name' => 'Fabriceeee bordel']);
     }
 }
