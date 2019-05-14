c134b7b87ca297bd716c97e19175d505185c5c72
Fabien Vauclin <fabien.vauclin@gmail.com>
2019-05-14 16:47:45 +0200

    Create fabien-function

--- src/Controller/indexController.php
+++ src/Controller/indexController.php
@@ -6,8 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class indexController extends AbstractController
 {
-    public function benoitXavier()
+    public function fabien()
     {
-        return $this->json(['name' => 'bx']);
+        return $this->json(['name' => 'fab']);
     }
 }
+
+
