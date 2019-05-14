3be1288e5b41138a4d22aa63d8a0d5c81b8772b0
Lyneeee <namiss.lyne@gmail.com>
2019-05-14 17:18:54 +0200

    modification of the function name

--- src/Controller/indexController.php
+++ src/Controller/indexController.php
@@ -6,8 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class indexController extends AbstractController
 {
+    public function benoitXavier()
+    {
+        return $this->json(['name' => 'bx']);
+    }
+
     public function Lyne()
     {
-        return $this->json(['name' => 'Lyneeee']);
+        return $this->json(['name' => 'lyne']);
     }
+    
 }
