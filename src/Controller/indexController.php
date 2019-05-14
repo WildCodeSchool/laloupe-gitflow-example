7e6b62c93dce2a3fb107ed9feee7675ff3739cf5
Richartrex <learic7@gmail.com>
2019-05-14 17:05:18 +0200

    resolved conflict in function richard

--- src/Controller/indexController.php
+++ src/Controller/indexController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class indexController extends AbstractController
 {
+    public function benoitXavier()
+    {
+        return $this->json(['name' => 'bx']);        
+    }
     public function richard()
     {
         return $this->json(['name' => 'richard']);
