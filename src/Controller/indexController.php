2d3db09ac0cdb49bd770c72a4af218e4858f52b2
Thibault Durand <thibault.informatique@gmail.com>
2019-05-14 16:50:59 +0200

    Integration of Thibdr Function

--- src/Controller/indexController.php
+++ src/Controller/indexController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class indexController extends AbstractController
 {
-    public function benoitXavier()
+    public function thibdr()
     {
-        return $this->json(['name' => 'bx']);
+        return $this->json(['name' => 'thibdr']);
     }
 }
