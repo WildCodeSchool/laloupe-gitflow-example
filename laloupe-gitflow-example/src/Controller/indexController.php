4deebb9d0e98eff5e65b1f6bafb828a451544666
Antoine FAINTRENIE <antoinefaintrenie@gmail.com>
2019-05-14 17:15:53 +0200

    correction the public function inside the indexController.php

--- laloupe-gitflow-example/src/Controller/indexController.php
+++ laloupe-gitflow-example/src/Controller/indexController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class indexController extends AbstractController
 {
+    public function benoitXavier()
+    {
+        return $this->json(['name' => 'bx']);
+    }
+
     public function antoineFaintrenie()
     {
         return $this->json(['name' => 'af']);
