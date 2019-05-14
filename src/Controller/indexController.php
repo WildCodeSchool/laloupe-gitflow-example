50b80c07900d55ce3693215d5dd8083560c54241
Quentin Moreau <qdr.moreau@yahoo.fr>
2019-05-14 16:46:55 +0200

    Edit function of controller for change bx to quentin

--- src/Controller/indexController.php
+++ src/Controller/indexController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class indexController extends AbstractController
 {
-    public function benoitXavier()
+    public function quentin()
     {
-        return $this->json(['name' => 'bx']);
+        return $this->json(['name' => 'quentin']);
     }
 }
